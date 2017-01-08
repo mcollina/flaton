d76ec2a14213d0e65ad1adf1d4cd38476a25472f
Matteo Collina <hello@matteocollina.com>
2017-01-08 13:14:02 +0100

    Added growing the internal buffer to RW.

--- test/rw.js
+++ test/rw.js
@@ -19,3 +19,20 @@ test('encode and decode a string', (t) => {
 
   t.end()
 })
+
+test('encode and decode a big string', (t) => {
+  const w = RW()
+  const str = Buffer.allocUnsafe(2048).fill('a').toString()
+  w.writeString(str)
+
+  t.ok(w.asBuffer() instanceof Buffer, 'asBuffer returns a buffer')
+  t.ok(w.asUint8Array() instanceof Uint8Array, 'asBuffer returns a buffer')
+
+  t.equal(w.asBuffer().length, Buffer.byteLength(str) + 4, 'Buffer length matches')
+  t.equal(w.asUint8Array().length, str.length + 4, 'Uint8Array length matches')
+
+  const r = RW(w.asBuffer())
+  t.equal(r.read(), str)
+
+  t.end()
+})
